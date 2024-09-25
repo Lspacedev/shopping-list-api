828c8b7d87be38ffa4bce2201c498b007a6c3e4b
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-09-25 15:59:26 +0200

    updated server

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   let endPoint = req.url;
   let method = req.method;
-
+  console.log(req);
   if (endPoint === "/lists" && method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
 
     // read json file and return all lists
-    fs.readFile("./lists.json", "utf8", (err, jsonString) => {
+    fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
       if (err) {
         console.error(err);
         res.end();
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
 
-    fs.readFile("./lists.json", "utf8", (err, jsonString) => {
+    fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
       if (err) {
         console.error(err);
         res.end();
@@ -41,28 +41,33 @@ const server = http.createServer((req, res) => {
             body += data;
           })
           .on("end", () => {
-            console.log(body);
-          });
+            let val = getInputValue(body);
+            let obj = arrayToObject(val);
 
-        // let updatedLists = JSON.stringify(lists);
-        // //write to json
-        // fs.writeFile("./lists.json", updatedLists, (err) => {
-        //   if (err) {
-        //     res.end("Error writing file", err);
-        //   } else {
-        //     res.end("Successfully wrote file");
-        //   }
-        // });
+            lists.push(obj);
+
+            let updatedLists = JSON.stringify(lists);
+            //write to json
+            fs.writeFile("./storage/lists.json", updatedLists, (err) => {
+              if (err) {
+                console.error("Error writing file", err);
+                res.end();
+              } else {
+                console.log("Successfully added data");
+                res.end();
+              }
+            });
+          });
       } catch (err) {
         console.error(err);
         res.end();
       }
     });
-    //post data to json file
   }
   if (endPoint === "/lists/:listId" && method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
+    console.log(req);
     //use params to get list id
     //read json file and return specific list
 
@@ -87,7 +92,25 @@ const server = http.createServer((req, res) => {
   // res.write(JSON.stringify(countries));
   // res.end();
 });
-
+function getInputValue(str) {
+  let val = str.replace(/[^a-z0-9-]/gi, ",");
+  return val.split(",");
+}
+function arrayToObject(arr) {
+  let obj = {};
+  arr.forEach((element, i) => {
+    if (element === "itemName") {
+      obj.itemName = arr[i + 1];
+    }
+    if (element === "category") {
+      obj.category = arr[i + 1];
+    }
+    if (element === "quantity") {
+      obj.quantity = arr[i + 1];
+    }
+  });
+  return obj;
+}
 server.listen(3000, "localhost", () => {
   console.log("listening for requests on port 3000");
 });
