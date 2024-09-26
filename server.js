5bcbda5c79334f2ef2838e1099cd3d0fd75cb7e5
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-09-26 15:57:06 +0200

    working on images

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const http = require("http");
+const http = require("node:http");
 const fs = require("fs");
+const { url } = require("inspector");
+const { v4: uuidv4 } = require("uuid");
 // GET    /lists
 // POST   /lists
 // GET    /lists/:listId
@@ -9,24 +11,25 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   let endPoint = req.url;
   let method = req.method;
-  console.log(req);
+
   if (endPoint === "/lists" && method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-
     // read json file and return all lists
     fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
       if (err) {
         console.error(err);
         res.end();
       }
+
       res.end(jsonString);
     });
   }
+
   if (endPoint === "/lists" && method === "POST") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-
+    // console.log("REQ FILES", req);
     fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
       if (err) {
         console.error(err);
@@ -41,9 +44,17 @@ const server = http.createServer((req, res) => {
             body += data;
           })
           .on("end", () => {
-            let val = getInputValue(body);
-            let obj = arrayToObject(val);
-
+            let val;
+            let obj = {};
+            console.log({ body });
+            if (body.includes("Content-Disposition")) {
+              obj = parseFormData(body);
+              console.log(obj);
+            } else {
+              val = getInputValue(body);
+              obj = arrayToObject(val);
+            }
+            obj.id = uuidv4();
             lists.push(obj);
 
             let updatedLists = JSON.stringify(lists);
@@ -64,40 +75,145 @@ const server = http.createServer((req, res) => {
       }
     });
   }
-  if (endPoint === "/lists/:listId" && method === "GET") {
+  //sub routes
+  let subRoutes = req.url.split("/");
+  let listId = subRoutes[2];
+  console.log(listId);
+  if (typeof listId !== "undefined" && method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    console.log(req);
-    //use params to get list id
-    //read json file and return specific list
 
-    res.end();
+    //read json file and return specific list using id
+    fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      }
+      try {
+        let lists = JSON.parse(jsonString);
+        let [list] = lists.filter((list) => list.id === listId);
+        console.log({ list });
+        if (typeof list !== "undefined") {
+          if (JSON.stringify(list) !== "{}") {
+            res.end(JSON.stringify(list));
+          }
+        } else {
+          res.end("Item not found");
+        }
+      } catch (err) {
+        console.error(err);
+        res.end();
+      }
+    });
   }
-  if (endPoint === "/lists/:listId" && method === "PUT") {
+
+  if (typeof listId !== "undefined" && method === "PUT") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    //use params to get list id
-    //update json file and return specific list
 
-    res.end();
+    //read json file and return specific list using id
+    fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      }
+      try {
+        let lists = JSON.parse(jsonString);
+        let list = lists.find((list) => list.id === listId);
+
+        if (typeof list !== "undefined") {
+          if (JSON.stringify(list) !== "{}") {
+            let body = "";
+            req
+              .on("data", (data) => {
+                body += data;
+              })
+              .on("end", () => {
+                let val;
+                let obj = {};
+                if (body.includes("Content-Disposition")) {
+                  obj = parseFormData(body);
+                } else {
+                  val = getInputValue(body);
+                  obj = arrayToObject(val);
+                }
+                if (typeof obj.itemName !== "undefined") {
+                  list.itemName = obj.itemName;
+                }
+                if (typeof obj.category !== "undefined") {
+                  list.category = obj.category;
+                }
+                if (typeof obj.quantity !== "undefined") {
+                  list.quantity = obj.quantity;
+                }
+                let updatedLists = JSON.stringify(lists);
+                //write to json
+                fs.writeFile("./storage/lists.json", updatedLists, (err) => {
+                  if (err) {
+                    console.error("Error writing file", err);
+                    res.end();
+                  } else {
+                    res.end("Successfully added data");
+                  }
+                });
+              });
+          }
+        } else {
+          res.end("Item not found");
+        }
+      } catch (err) {
+        console.error(err);
+        res.end();
+      }
+    });
   }
-  if (endPoint === "/lists/:listId" && method === "DELETE") {
+
+  if (typeof listId !== "undefined" && method === "DELETE") {
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    //use params to get list id
-    //delete json file and return specific list
 
-    res.end();
+    //read json file and return specific list using id
+    fs.readFile("./storage/lists.json", "utf8", (err, jsonString) => {
+      if (err) {
+        console.error(err);
+        res.end();
+      }
+      try {
+        let lists = JSON.parse(jsonString);
+        let list = lists.filter((list) => list.id === listId);
+
+        if (typeof list !== "undefined") {
+          if (JSON.stringify(list) !== "{}") {
+            let filteredLists = lists.filter((list) => list.id !== listId);
+            let updatedLists = JSON.stringify(filteredLists);
+            //write to json
+            fs.writeFile("./storage/lists.json", updatedLists, (err) => {
+              if (err) {
+                console.error("Error writing file", err);
+                res.end();
+              } else {
+                res.end("Successfully deleted data");
+              }
+            });
+          }
+        } else {
+          res.end("Item not found");
+        }
+      } catch (err) {
+        console.error(err);
+        res.end();
+      }
+    });
   }
-  // res.write(JSON.stringify(countries));
-  // res.end();
 });
 function getInputValue(str) {
   let val = str.replace(/[^a-z0-9-]/gi, ",");
+  console.log(val);
   return val.split(",");
 }
 function arrayToObject(arr) {
   let obj = {};
+
   arr.forEach((element, i) => {
     if (element === "itemName") {
       obj.itemName = arr[i + 1];
@@ -108,7 +224,51 @@ function arrayToObject(arr) {
     if (element === "quantity") {
       obj.quantity = arr[i + 1];
     }
+    if (element.includes("img")) {
+      var arrayBufferView = new Uint8Array(arr[i + 3]);
+      var blob = new Blob([arrayBufferView], { type: "image/jpeg" });
+      // let part1 = arr[i + 2];
+      // let part2 = arr[i + 3];
+      // let part3 = arr[i + 4];
+      // let img = part1 + part2;
+      // fs.writeFile(
+      //   "packageName.png",
+      //   Buffer.from(part2, "binary"),
+      //   function (err) {
+      //     if (err) throw err;
+      //     console.log("success");
+      //   }
+      // );
+
+      // let my_uint8_array = Uint8Array.from(arr[i + 3], (c) => c.charCodeAt(0));
+
+      // let blob = new Blob([my_uint8_array], { type: "image/png" });
+
+      // let myUrl = URL.createObjectURL(blob);
+      // obj.img = myUrl;
+    }
   });
+  return obj;
+}
+function parseFormData(str) {
+  let val = str.split("\r\n");
+  console.log({ val });
+  //get img
+
+  val = val.filter((valStr) => !valStr.match(/^-/gi));
+
+  val = val.filter((valStr) => valStr !== "");
+
+  val = val.map((valStr) => {
+    if (valStr.includes("Content")) {
+      let str = valStr.substring(valStr.indexOf("=") + 1);
+      str = str.substring(1, str.length - 1);
+      return str;
+    }
+    return valStr;
+  });
+
+  let obj = arrayToObject(val);
   return obj;
 }
 server.listen(3000, "localhost", () => {
